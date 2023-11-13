bd1528d49b6531f984b7ddfc7ea6758c19b6b9c3
Tonalli R.-L <t3rodrig@users.noreply.github.com>
2023-11-12 19:13:41 -0600

    Do 7.3 Creating a Grid Layout

--- app_02-soccer.py
+++ app_02-soccer.py
@@ -4,19 +4,20 @@ import dash_bootstrap_components as dbc
 
 soccer = pd.read_csv('data/fifa_soccer_players.csv')
 
-app = Dash(external_stylesheets=[dbc.themes.CYBORG])
+app = Dash(external_stylesheets=[dbc.themes.GRID])
 
 app.layout = html.Div([
     html.H1('Soccer Players Dashboard'),
-    html.P([
-        html.A('Sofifa',
-               href='https://sofifa.com',
-               target='_blank')]),
-    html.Label('Player name: '),
-    dcc.Dropdown(
-        options=soccer['long_name'].unique(),
-        value=soccer['long_name'].unique()[0]
-    )
+    dbc.Row([
+        dbc.Col(html.P(['Source: ',
+                        html.A('Sofifa',
+                               href='https://sofifa.com',
+                               target='_blank')])),
+        dbc.Col([html.Label('Player name: '),
+                 dcc.Dropdown(
+                     options=soccer['long_name'].unique(),
+                     value=soccer['long_name'].unique()[0]
+                 )])])
 ])
 
 if __name__ == '__main__':
