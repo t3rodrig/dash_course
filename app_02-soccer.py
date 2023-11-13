96e1daadcde667f6fc9fce07fb8d6040469ce275
Tonalli R.-L <t3rodrig@users.noreply.github.com>
2023-11-12 19:31:53 -0600

    Do 7.4 Building a Navigation Bar

--- app_02-soccer.py
+++ app_02-soccer.py
@@ -2,23 +2,23 @@ import pandas as pd
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-soccer = pd.read_csv('data/fifa_soccer_players.csv')
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app = Dash(external_stylesheets=[dbc.themes.GRID])
+navbar = dbc.NavbarSimple(
+    brand='Soccer Players Dashboard',
+    children=[
+        html.Img(src='https://uptime.com/media/website_profiles/sofifa.com.png',
+                 height=20),
+        html.A('Data Source',
+               href='https://sofifa.com',
+               target='_blank',
+               style={'color': 'black'})
+    ],
+    color='primary',
+    fluid=True
+)
 
-app.layout = html.Div([
-    html.H1('Soccer Players Dashboard'),
-    dbc.Row([
-        dbc.Col(html.P(['Source: ',
-                        html.A('Sofifa',
-                               href='https://sofifa.com',
-                               target='_blank')])),
-        dbc.Col([html.Label('Player name: '),
-                 dcc.Dropdown(
-                     options=soccer['long_name'].unique(),
-                     value=soccer['long_name'].unique()[0]
-                 )])])
-])
+app.layout = html.Div(navbar)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
