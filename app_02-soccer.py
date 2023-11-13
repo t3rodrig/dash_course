d03598442b783d357a5e5c6815edbb58eae7ef83
Tonalli R.-L <t3rodrig@users.noreply.github.com>
2023-11-12 18:06:02 -0600

    Do 7.1 Customizing with Inline CSS

---
+++ app_02-soccer.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from dash import Dash, html, dcc
+
+soccer = pd.read_csv('data/fifa_soccer_players.csv')
+
+app = Dash()
+
+app.layout = html.Div([
+    html.H1('Soccer Players Dashboard',
+            style={'textAlign': 'center',
+                   'fontFamily': 'fantasy',
+                   'fontSize': 50,
+                   'color': 'blue'}),
+    html.P([
+        html.A('Sofifa',
+               href='https://sofifa.com',
+               target='_blank')],
+        style={'border': 'solid'}),
+    html.Label('Player name: '),
+    dcc.Dropdown(
+        options=soccer['long_name'].unique(),
+        value=soccer['long_name'].unique()[0],
+        style={'backgroundColor': 'lightblue'}
+    )
+], style={'padding': 100, 'border': 'solid'})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
