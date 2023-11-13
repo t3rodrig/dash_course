ee2226c8772604fa26c9a338f769acbb44770c9b
Tonalli RL <t3rodrig@users.noreply.github.com>
2023-11-13 15:50:07 -0600

    Do 7.5 Building a Deck of Cards

--- app_02-soccer.py
+++ app_02-soccer.py
@@ -2,7 +2,29 @@ import pandas as pd
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+soccer = pd.read_csv('data/fifa_soccer_players.csv')
+
+avg_age = soccer['age'].mean()
+avg_height = soccer['height_cm'].mean()
+avg_weight = soccer['weight_kg'].mean()
+
+cards = dbc.Row([
+    dbc.Col(dbc.Card([
+        html.H4('Avg. Age'),
+        html.H5(f'{round(avg_age, 1)} years')
+    ], body=True, style={'textAlign': 'center', 'color': 'white'},
+        color='lightblue')),
+    dbc.Col(dbc.Card([
+        html.H4('Avg. Height'),
+        html.H5(f'{round(avg_height, 1)} cm')
+    ], body=True, style={'textAlign': 'center', 'color': 'white'},
+        color='blue')),
+    dbc.Col(dbc.Card([
+        html.H4('Avg. weight'),
+        html.H5(f'{round(avg_weight, 1)} kg')
+    ], body=True, style={'textAlign': 'center', 'color': 'white'},
+        color='darkblue'))
+])
 
 navbar = dbc.NavbarSimple(
     brand='Soccer Players Dashboard',
@@ -18,7 +40,8 @@ navbar = dbc.NavbarSimple(
     fluid=True
 )
 
-app.layout = html.Div(navbar)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([navbar, html.Br(), cards])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
