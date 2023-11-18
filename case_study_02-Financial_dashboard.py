c637796de82659fc2e17c37dcab734314b0678c0
Tonalli RL <t3rodrig@users.noreply.github.com>
2023-11-17 22:23:31 -0600

    Do 9.2 Setting Up the Layout_
    Candlestick and DataTable

--- case_study_02-Financial_dashboard.py
+++ case_study_02-Financial_dashboard.py
@@ -1,5 +1,15 @@
-from dash import Dash, html, dcc
+import yfinance as yf
+from dash import Dash, html, dcc, dash_table
+import plotly.graph_objects as go
 
+price = yf.Ticker('AAPL').history(period='1d', interval='15m').reset_index()
+fig = go.Figure(data=go.Candlestick(
+    x=price['Datetime'],
+    open=price['Open'],
+    high=price['High'],
+    low=price['Low'],
+    close=price['Close']
+))
 app = Dash()
 
 app.layout = html.Div([
@@ -7,7 +17,9 @@ app.layout = html.Div([
     dcc.Input(id='ticker-input',
               placeholder='Search for symbols from Yahoo Finance',
               style={'width': '50%'}),
-    html.Button(id='submit-button', children='Submit')
+    html.Button(id='submit-button', children='Submit'),
+    dcc.Graph(id='stock-graph', figure=fig),
+    dash_table.DataTable(id='stock-data', data=price.tail(10).to_dict('records'))
 ])
 
 if __name__ == '__main__':
